23b2ffa10ef8ef2d44dfc851942764306e0aa2c8
antoniolemos34-code <antoniolemos34@gmail.com>
2026-02-20 22:20:26 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
   function track(name, data){
     try{ console.log("[track]", name, data || {}); }catch(e){}
   }
+
   document.querySelectorAll("[data-track]").forEach(function(el){
     el.addEventListener("click", function(){
       track(el.getAttribute("data-track"), {href: el.getAttribute("href") || null});
     });
   });
+
   var form = document.querySelector("form[data-form]");
   if(form){
     form.addEventListener("submit", function(){
